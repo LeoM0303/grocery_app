356e1405a00b0405ace079fc4f8490f5d76cd0b5
LeoM0303 <leomuliarchuk51@gmail.com>
2025-07-02 16:44:57 +0300

    add delete

--- backend/prod_dao.py
+++ backend/prod_dao.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from sql_connection import get_sql_connection
 
 def get_all_prod(connection):
@@ -21,6 +23,27 @@ def get_all_prod(connection):
        )
 
     return response
+
+def insert_new_prod(connection, product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO products (name, uom_id, price_per_unit) "
+             "VALUES (%s, %s, %s)")
+    data = (product['product_name'], int(product['uom_id']), float(product['price_per_unit']))
+    cursor.execute(query, data)
+    connection.commit()
+
+    return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    queue = ("DELETE FROM products where products_id=" + str(product_id))
+    cursor.execute(query)
+    connection.commit()
+
 if __name__ == '__main__':
     connection = get_sql_connection()
-    print(get_all_prod(connection))
+    print(insert_new_prod(connection, {
+        'product_name': 'cabbage',
+        'uom_id': 1,
+        'price_per_unit': 10
+    }))
